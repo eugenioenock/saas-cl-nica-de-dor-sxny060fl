fa2a92b740104587e93e3d92ec1d56e6348078e8
Skip <skip@usecurling.com>
2026-04-17 23:12:52 +0000

    Fix "Prontuários" navigation and 404 error

--- src/pages/RecordsRedirect.tsx
+++ src/pages/RecordsRedirect.tsx
@@ -2,5 +2,5 @@ import { Navigate } from 'react-router-dom'
 
 export default function RecordsRedirect() {
   // Simple redirect to patients list since records are accessed via patient
-  return <Navigate to="/patients" replace />
+  return <Navigate to="/pacientes" replace />
 }
